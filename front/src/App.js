95ff53e27e34e8ef64506d2f5677b5b0460bd343
Tarcisio <caninosbrancos10031@gmail.com>
2023-01-30 22:17:31 -0300

    components

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: 'center' }}>Hooks</h1>
+
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/Components/Home/index.jsx
@@ -0,0 +1,11 @@
+import { Div } from './styled';
+
+export default function Home() {
+  return (
+    <Div>
+      <div className="container">
+        <h1>home</h1>;
+      </div>
+    </Div>
+  );
+}
